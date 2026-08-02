70dae0e572d7dc4a334acd75553183f84a394751
Aleksandra Semenenia <ashaikeus@gmail.com>
2026-08-03 00:17:02 +0200

    feat: set schema defaults

--- src/schemas/downloads.py
+++ src/schemas/downloads.py
@@ -1,7 +1,7 @@
-from datetime import datetime
-from uuid import UUID
+from datetime import datetime, timezone
+from uuid import UUID, uuid4
 
-from pydantic import BaseModel, HttpUrl
+from pydantic import BaseModel, Field, HttpUrl
 
 from enums import DownloadStatus
 
@@ -13,11 +13,11 @@ class DownloadCreate(BaseModel):
 
 
 class DownloadCreated(BaseModel):
-    request_id: UUID
-    created_at: datetime
-    status: DownloadStatus
+    request_id: UUID = Field(default_factory=uuid4)
+    created_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
+    status: DownloadStatus = DownloadStatus.QUEUED
 
 
 class DownloadDetail(DownloadCreate, DownloadCreated):
-    completed_at: datetime | None
-    file_link: HttpUrl | None
+    completed_at: datetime | None = None
+    file_link: HttpUrl | None = None
