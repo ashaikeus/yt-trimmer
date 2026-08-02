4eb781f1908867eb266c4415848ea84d5bbedd82
Aleksandra Semenenia <ashaikeus@gmail.com>
2026-08-03 00:17:56 +0200

    fix: get enum values

--- src/services/post_downloads_service.py
+++ src/services/post_downloads_service.py
@@ -38,11 +38,11 @@ def background_download(download: DownloadDetail) -> None:
         with YoutubeDL(YDL_LOGGING_OPTIONS) as ydl:
             ydl.download(str(download.youtube_link))
         redis_client.hset(
-            f"download:{download.request_id}", "status", DownloadStatus.COMPLETED
+            f"download:{download.request_id}", "status", DownloadStatus.COMPLETED.value
         )  # todo: create repository
         logger.info(f"[{download.request_id}]: file downloaded")
     except DownloadError as exc:
         redis_client.hset(
-            f"download:{download.request_id}", "status", DownloadStatus.FAILED
+            f"download:{download.request_id}", "status", DownloadStatus.FAILED.value
         )
         logger.exception(f"[{download.request_id}]: download failed - {exc}")
