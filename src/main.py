843b55e8d5b66cda667735281a9d37b7b8d11171
Aleksandra Semenenia <ashaikeus@gmail.com>
2026-08-03 00:21:42 +0200

    chore: remove comment

--- src/main.py
+++ src/main.py
@@ -1,18 +1,3 @@
-# Goal for today: save YouTube video from a provided link
-
-# API:
-# - POST /downloads
-#   send a downloading request
-#   BODY PARAMS:
-#   * youtube_link
-#   - start_trim
-#   - end_trim
-# - GET /downloads/{request_id}
-#   poll for status, returns download link
-# - GET /files/{file_id}
-#   the downloading endpoint when the request completes successfully
-
-
 from fastapi import FastAPI
 
 from routers import download_router
